c74f00e998697493987a1154f0bc6a0e50e12911
Sahil <sk4386840@gmail.com>
2025-06-06 12:51:42 +0530

    setup route file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
-app.use(/api/auth, authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(5001,()=>{
     console.log("server listening on port 5001");
