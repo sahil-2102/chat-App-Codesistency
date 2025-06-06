ffe58808e1aadb94274ff0bbaf59af87b262977e
Sahil <sk4386840@gmail.com>
2025-06-06 15:20:15 +0530

    completed signup logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,39 @@
-export const signup = (req, res) => {
-    res.send("signup route");
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+    const {fullname, email, password} = req.body;
+    try {
+        if(password.length < 6){
+           return res.status(400).json({message: "Password should be more than or equal to 6 characters!"});
+        }
+        const existingUser = await User.findOne({email});
+        if(existingUser){ return res.statusCode(400).json({message: "User already exists!"});}
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = new User({
+            fullname,
+            email,
+            password: hashedPassword
+        })
+        if (newUser) {
+            generateToken(newUser._id, res);
+            res.status(200).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                email:newUser.email,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({message: "Invalid user data!"});
+        }
+
+    } catch (error) {
+        res.status(400).json({message: "Internal server error"});
+    }
 }
+
+
 export const login = (req, res) => {
     res.send("login route");
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 connectDB();
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT,()=>{
