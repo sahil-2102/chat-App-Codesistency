eaef757e8c43699a79da63076504979223acdd5e
Sahil <sk4386840@gmail.com>
2025-06-07 13:47:18 +0530

    minor changes done and authCheck route created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieparser from "cookie-parser"
 import dotenv from "dotenv";
 dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cookieparser());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT,()=>{
