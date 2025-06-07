ec092e50dfb28b5ff5b76dbcc3116de3b41fad84
Sahil <sk4386840@gmail.com>
2025-06-07 12:16:16 +0530

    minor changes in protect route

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,9 +1,14 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 export const protect = async (req, res, next) => {
-    const token = req.cookies.token;
+  try {
+    const token = req.cookies.jwt;
     if (!token) return res.status(401).json({ message: "Not authorized" });
     const decode = jwt.verify(token, process.env.jwt_SECRET);
     req.user = await User.findById(decode.userId).select("-password");
     next();
-}
+  } catch (error) {
+    console.log("Error in logout controller! ", error.message);
+    res.status(500).json({ message: "Internal server error!" });
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.post("/signup", signup);
 router.post('/login', login);
 router.post('/logout', logout);
+router.put("/update",protect);
 
 export default router;
