ccab618182c9adc11646786e3641fdb57cf99408
Sahil <sk4386840@gmail.com>
2025-06-08 15:44:31 +0530

    initial setup of client routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import Navbar from "./components/Navbar.jsx";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/Homepage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 const App = () => {
   return (
     <div>
-      
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingsPage/>}/>
+        <Route path="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   );
 };
