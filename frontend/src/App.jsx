39dd259bbbcb16001c5bf21c18f6f0bbb8b65975
Sahil <sk4386840@gmail.com>
2025-06-08 15:26:44 +0530

    installation of daisyUI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div>
+      
+    </div>
+  );
+};
+export default App;
