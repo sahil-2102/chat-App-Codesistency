79f95dee16a497b4e8c8f4753cd481cc3333fd62
Sahil <sk4386840@gmail.com>
2025-06-09 12:45:03 +0530

    implementation of loading login using with lucide react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,19 @@ import LoginPage from "./pages/LoginPage.jsx";
 import SettingsPage from "./pages/SettingsPage.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import { useAuthStore } from "./store/userAuthStore.js";
+import { useEffect } from "react";
+import {Loader} from "lucide-react";
 const App = () => {
-  const {authUser, checkAuth} = useAuthStore();
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className= "size-10 animate-spin"/>
+    </div>
+  )
   return (
     <div>
       <Navbar/>
--- frontend/src/store/userAuthStore.js
+++ frontend/src/store/userAuthStore.js
@@ -1,6 +1,6 @@
 import {create} from "zustand";
 import { axiosInstance } from "../lib/axios.js";
-export const useAuthStore = create(() => ({
+export const useAuthStore = create((set) => ({
     authUser: null,
     isSigningUp: false,
     isLoggingIn: false,
