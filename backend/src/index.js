e583675b4d97b51672e28c91092bdf98bb703ba0
Sahil <sk4386840@gmail.com>
2025-06-09 12:15:30 +0530

    cors setup completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,17 @@ import { connectDB } from "./lib/db.js";
 import cookieparser from "cookie-parser"
 import dotenv from "dotenv";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 dotenv.config();
 connectDB();
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieparser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
